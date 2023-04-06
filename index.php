04adad9d739688c4d750378bb5fc9649e3ff43e2
Ajtech001 <Oguncoder11@yahoo.com>
2023-04-06 10:48:37 -0500

    style css and index html

---
+++ index.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Chat Application</title>
+    <script src="https://kit.fontawesome.com/1d2533baae.js" crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="./style.css">
+</head>
+<body>
+    <di class="wrapper">
+        <section class="form signup">
+            <header>RealTime ChatApp</header>
+            <form action="#">
+                <div class="error-txt">This is an error message</div>
+                <div class="name-details">
+                    <div class="field">
+                        <label for="">Firstname</label>
+                        <input type="text" placeholder="enter firstname">
+                    </div>
+                    <div class="field">
+                        <label for="">Lastname</label>
+                        <input type="text" placeholder="enter lastname">
+                    </div>
+                    </div>
+                    <div class="field">
+                        <label for="">Email</label>
+                        <input type="text" placeholder="enter email">
+                    </div>
+                    <div class="field">
+                        <label for="">Firstname</label>
+                        <input type="text" placeholder="enter fnew password">
+                    </div>
+                    <div class="field">
+                        <label for="">Select Image</label>
+                        <input type="file">
+                    </div>
+                    <div class="field">
+                        <input type="submit" value="continue to chat">
+                    </div>
+            </form>
+            <div class="link">Already signed up? <a href="#">Login</a></div>
+        </section>
+    </di>
+</body>
+</html>
